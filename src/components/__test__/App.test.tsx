bbd4a627c6f0b7b2777a122eb35f78e3f8d94213
Maksim Sukhov <silveraven@yandex.ru>
2025-07-21 01:47:42 +0200

    test: add demo test case in App

--- src/components/__test__/App.test.tsx
+++ src/components/__test__/App.test.tsx
@@ -1,11 +1,3 @@
-import App from '../../App';
-import { render } from '@testing-library/react';
-
 test('demo', () => {
   expect(true).toBe(true);
-});
-
-test('Renders the main page', () => {
-  render(<App />);
-  expect(true).toBeTruthy();
 });
